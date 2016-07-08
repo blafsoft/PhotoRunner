9b5813ea7430bb72ec9a8b9874bac164127ec2ff
manjeetanaad <Manjeetsingh114411@gmail.com>
2016-07-08 06:13:00 +0200

    Update head-other.php

--- include/head-other.php
+++ include/head-other.php
@@ -18,6 +18,13 @@
 	<!-- start-smoth-scrolling -->
 	<script type="text/javascript" src="<?php echo APP_URL; ?>js/move-top.js"></script>
 	<script type="text/javascript" src="<?php echo APP_URL; ?>js/easing.js"></script>
+	
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+
+
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>js/base.css" />
+	<link rel="stylesheet" href="<?php echo APP_URL; ?>js/style.css" />
 	<script type="text/javascript">
 		jQuery(document).ready(function($) {
 			$(".scroll").click(function(event){		
@@ -50,4 +57,11 @@
 	$("[data-toggle=tooltip]").tooltip();
 	});
 </script>
+	<style>
+	.abcd {
+	    background-image: url("images/logo.png");
+	    background-size:100%;
+	    width:100%;
+	}
+	</style>
 
