da17fccb5f4f97853138987a8b2feb678fa0e7fb
manjeetanaad <Manjeetsingh114411@gmail.com>
2016-07-11 14:14:35 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ $stripe = array(
   //"secret_key"      => "sk_live_f0Zb65LFbH3jQV4zGbzb1AMV",
   //"publishable_key" => "pk_live_obp9GmiNdWLl0DWA5wYW6rCv"
 
-  "secret_key"      => "sk_test_YD7LxyFy5N4k2wThJACJJ3rh",
-  "publishable_key" => "pk_test_aPEmcSGdAZrf0EssMbHa1QRy"
+  "secret_key"      => "sk_test_xitA2poC7TfjnP1IGD0FT6rp",
+  "publishable_key" => "pk_test_inM99ehBADdrzRTf3wa3ggu2"
 );
 
 Stripe::setApiKey($stripe['secret_key']);
