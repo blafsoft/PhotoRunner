824f74fceffabcb9b8b052c5cca3821da2b0c109
manjeetanaad <Manjeetsingh114411@gmail.com>
2016-08-02 12:07:21 +0200

    Update download.php

--- download.php
+++ download.php
@@ -6,7 +6,7 @@ if(isset($_POST['downloadwebfile']))
 	$downloadfile = $common->getrecord('pr_photos','*',$condition);
 
 	$downloadf = $downloadfile->webfile;
-	$file = "https://s3-eu-west-1.amazonaws.com/photorunner.download/$downloadf";
+	$file = "http://www.photorunner.no/uploads/photos/real//$downloadf";
 	if (file_exists($file)) {
 	    header('Content-Description: File Transfer');
 	    header('Content-Type: application/octet-stream');
