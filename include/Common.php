254638c9df0fba2dd819e426ec58c6db46c3ff1a
Frode Bjerkenes <frode.bjerkenes@gmail.com>
2016-08-19 23:09:28 +0200

    removed unused variables

--- include/Common.php
+++ include/Common.php
@@ -1775,16 +1775,9 @@ if((empty($email)) || (empty($username))  || (empty($about))  || (empty($area)) 
 		$printfilepricea4euro = mysqli_real_escape_string( $this->_con, $trimmed_data['printfilepricea4euro'] );
 		$printfilepricea5euro = mysqli_real_escape_string( $this->_con, $trimmed_data['printfilepricea5euro'] );
 
-		$webfilepricea3euro = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepricea3euro'] );
-		$webfilepricea4euro = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepricea4euro'] );
-		$webfilepricea5euro = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepricea5euro'] );
-		$webfilepricea5euro = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepricea5euro'] );
 		$webfilepriceeuro = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepriceeuro'] );
 		$otherpriceeuro = mysqli_real_escape_string( $this->_con, $trimmed_data['otherpriceeuro'] );
 
-		$webfilepricea3 = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepricea3'] );
-		$webfilepricea4 = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepricea4'] );
-		$webfilepricea5 = mysqli_real_escape_string( $this->_con, $trimmed_data['webfilepricea5'] );
 
 		$imagewidth = mysqli_real_escape_string( $this->_con, $trimmed_data['imagewidth'] );
 		$imageheight = mysqli_real_escape_string( $this->_con, $trimmed_data['imageheight'] );
