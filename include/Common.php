a205bd4d4ff775edc538656adbfe9c85aeacf0b9
Frode Bjerkenes <frode.bjerkenes@gmail.com>
2016-08-31 19:14:29 +0200

    removed unused code + url in email to photo-view.php

--- include/Common.php
+++ include/Common.php
@@ -1845,7 +1845,7 @@ if((empty($email)) || (empty($username))  || (empty($about))  || (empty($area)) 
 		$message ="<html><body>
 		<div style='color:#00A2B5; font-size:46px; font-weight:bold; margin:20px; font-family:arial;'>PhotoRunner</div>".
 		"<div style='color:#6B555A; border:1px solid #ccc; margin-top:30px; width:50%; margin-top:20px; margin-left:auto; margin-right:auto; padding:10px; padding-top:30px; font-size:16px; background-color:#F2F2F2; text-align:center; font-family:arial;'>To get in touch with new exciting things, just click on the button below<br/><br/>".
-		"<a href='".APP_URL."photos.php?gallery=".$code."&&email=".$email."&&password=".$password."' style='color:#fff; font-family:arial; text-decoration:none; font-size:22px; font-weight:bold; margin-bottom:20px; padding-bottom:10px;'><div style='width:250px; margin-left:auto; margin-right:auto; background-color:#00A2B5; height:50px; font-family:arial; border-radius:5px; padding-top:15px; padding-bottom:15px; margin-bottom:30px;'> Click Me !</div></a></div><br/><br/>".
+		"<a href='".APP_URL."view-photo.php?gallery=".$code."&&email=".$email."&&password=".$password."' style='color:#fff; font-family:arial; text-decoration:none; font-size:22px; font-weight:bold; margin-bottom:20px; padding-bottom:10px;'><div style='width:250px; margin-left:auto; margin-right:auto; background-color:#00A2B5; height:50px; font-family:arial; border-radius:5px; padding-top:15px; padding-bottom:15px; margin-bottom:30px;'> Click Me !</div></a></div><br/><br/>".
 
 		"<div style='font-size:16px; font-family:arial; text-align:center; color:#00A2B5;'>If you need any help, Please contact us at post@photorunner.no</div><br/>".
 		"<div style='font-size:14px; text-align:left;'>Team<br/>Photo Runner</div>".
