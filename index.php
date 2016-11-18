336a83b4d5311872ba851eb0211253f6cf6e4cea
Filip Kocevski <filip@blafsoft.local>
2016-11-18 09:08:17 +0100

    Test commit

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 	</style>
 </head>
 <body>
+	<!--test git commit-->
 	<?php include('include/header.php'); ?>
 	<div class="heightheight"></div>
 	<div class="banner-bottom" style="background-color:#eee;">
